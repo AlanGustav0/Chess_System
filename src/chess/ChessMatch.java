72200d704bd03d8e3c88bf80952ddb419bffdcac
AlanGustav0 <alangustavo946@gmail.com>
2021-05-10 08:04:02 -0300

    Improvement in board print

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -14,7 +14,7 @@ public class ChessMatch {
 
 	public ChessMatch() {
 		board = new Board(8, 8);
-		initialSetup(); 
+		initialSetup();
 	}
 
 	/*
@@ -35,21 +35,33 @@ public class ChessMatch {
 
 		return matrixChess;
 	}
-	
-	/*Este método será o responsável por receber as posições, enviar para o ChessPosition
-	Que faz a conversão de posição de matriz, para posição de tabuleiro.
+
+	/*
+	 * Este método será o responsável por receber as posições, enviar para o
+	 * ChessPosition Que faz a conversão de posição de matriz, para posição de
+	 * tabuleiro.
 	 */
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
-		board.placePiece(piece, new ChessPosition(column,row).toPosition());
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
-	
-	//Método responsável por inserir as peças no tabuleiro e iniciar o jogo
-	
+
+	// Método responsável por inserir as peças no tabuleiro e iniciar o jogo
+
 	private void initialSetup() {
-		placeNewPiece('b',6,new Rook(board,Color.WHITE));
-		placeNewPiece('e',8,new King(board,Color.BLACK));
-		placeNewPiece('e',1,new King(board,Color.WHITE));
-		
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 8, new King(board, Color.BLACK));
+
 	}
 
 }
