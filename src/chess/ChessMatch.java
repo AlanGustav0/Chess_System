1ac0d111dd06be5c31d5b18d3d1f88983bb91b5b
AlanGustav0 <alangustavo946@gmail.com>
2021-05-10 07:52:42 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -37,12 +36,19 @@ public class ChessMatch {
 		return matrixChess;
 	}
 	
+	/*Este método será o responsável por receber as posições, enviar para o ChessPosition
+	Que faz a conversão de posição de matriz, para posição de tabuleiro.
+	 */
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column,row).toPosition());
+	}
+	
 	//Método responsável por inserir as peças no tabuleiro e iniciar o jogo
 	
 	private void initialSetup() {
-		board.placePiece(new Rook(board,Color.WHITE), new Position(2,1));
-		board.placePiece(new King(board,Color.BLACK), new Position(0,4));
-		board.placePiece(new King(board,Color.WHITE), new Position(7,4));
+		placeNewPiece('b',6,new Rook(board,Color.WHITE));
+		placeNewPiece('e',8,new King(board,Color.BLACK));
+		placeNewPiece('e',1,new King(board,Color.WHITE));
 		
 	}
 
---
+++ src/chess/exceptions/ChessException.java
@@ -0,0 +1,13 @@
+package chess.exceptions;
+
+public class ChessException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ChessException(String message) {
+		super(message);
+	}
+	
+	
+
+}
